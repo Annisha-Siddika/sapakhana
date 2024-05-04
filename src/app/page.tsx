c68b4d07fd9f5f9985d5efb805cc4c5edf208459
AnnishaSiddika <annisha2112@gmail.com>
2024-05-04 15:34:56 +0100

    sonsoi font added and created the logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/modeToggle";
-import Image from "next/image";
+import Header from "@/components/homeComponents/header/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <nav>
-        <ModeToggle />
-      </nav>
-      <h1>Sapakhana With shadcn</h1>
-
-      <Button variant={"default"}>ShadCN Button</Button>
+      <Header/>
     </main>
   );
 }
---
+++ src/components/homeComponents/header/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { sonsoiOne } from '../../../app/layout'
+export default function Logo() {
+  return (
+    <div className={sonsoiOne.className}>
+        <h2 className='text-2xl'><span className='text-cyan-500'>Sapa</span><span className='text-pink-600'>Khana</span></h2>
+        
+    </div>
+  )
+}
