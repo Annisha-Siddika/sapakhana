a2a90bbe2b491b1110b280e095bb748f1bad5251
AnnishaSiddika <annisha2112@gmail.com>
2024-05-18 18:18:37 +0100

    inspiration section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Ad2 from "@/components/homeComponents/ads/Ad2";
 import DesignPicked from "@/components/homeComponents/designPicked/DsgnPicked";
 import Featured from "@/components/homeComponents/featured/Featured";
 import Hero from "@/components/homeComponents/hero/Hero";
+import Inspiration from "@/components/homeComponents/inspiration/Inspiration";
 import Categories from "@/components/homeComponents/pCategories/Categories";
 import TopSelling from "@/components/homeComponents/topSelling/TopSelling";
 
@@ -17,6 +18,7 @@ export default function Home() {
       <TopSelling/>
       <Featured/>
       <Categories/>
+      <Inspiration/>
     </main>
   );
 }
